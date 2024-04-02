3ccd0302b3dd807fd04f8b4f36be475da18d1b96
reifaldybarka <baerepal@gmail.com>
2024-04-02 16:00:08 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Reifaldy Barka Ramadhan
+              saya seorang Mahasiswa Univ BSI,
+              jurusan saya TI prodi Ilmu Komputer";
+    }
+}
---
+++ application/views/V_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Reifaldy</li>
+        <li>Nama Belakang : Barka Ramadhan</li>
+</ul>
+<br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Raya Dewi Sartika Depok</li>
+</ul>
+
+    <h4>Tempat lahir</h4>
+    <ul type="none">
+        <li>Depok</li>
+</ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Bulutangkis</li>
+</ul>
+</section>
